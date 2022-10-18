cfbc609003e2ec5162ba2aec835d67f6d88cf380
Lebogang Sekaleli <lebogangsekaleli@gmail.com>
2022-10-18 07:09:43 -0700

    Task 1

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 	while (i++ <= 9)
 	{
 
-	for (letter 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 		_putchar('\n');
 	}
