0933d707e812e50eef6631af22cf1d1a812d9786
Lebogang Sekaleli <lebogangsekaleli@gmail.com>
2022-10-19 09:23:50 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*add - adds two integers and returns the result
+*@n: int is n
+*Return: returns the result
+*/
+int add(int n)
+{
+	   int n;
+
+	   n = add(89, 9);
+	   printf("%d\n", n);
+	   return (0);
+}
