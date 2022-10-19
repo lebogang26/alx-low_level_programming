9c750e056b3f068d57720fd6d71150ad17709bbe
Lebogang Sekaleli <lebogangsekaleli@gmail.com>
2022-10-19 01:29:41 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,16 +2,17 @@
 /**
  * void print_alphabet_x10 - prints 10 times the alphabet in lowercase
  */
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 {
-	int i = 0;
-	char letter;
+	int i;
+	int x;
 
-	while (i++ <= 9)
+	for (x = 0; x < 10; x++)
 	{
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-		_putchar('\n');
+		for (i = 'a'; i <= 'z' i++)
+	{
+		_putchar(i);
+	}
+		_putchar(\n');
 	}
 }
